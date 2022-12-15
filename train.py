62066e326f7fb9b24908e8767d7431c302096b52
Xuan Hien <hiennguyen9874@gmail.com>
2022-12-15 02:30:10 +0000

    Update nbs

--- train.py
+++ train.py
@@ -162,7 +162,7 @@ def train(hyp, opt, device, tb_writer=None):
             v.requires_grad = False
 
     # Optimizer
-    nbs = 64  # nominal batch size
+    nbs = 32  # nominal batch size
     accumulate = max(round(nbs / total_batch_size), 1)  # accumulate loss before optimizing
     hyp["weight_decay"] *= total_batch_size * accumulate / nbs  # scale weight_decay
     logger.info(f"Scaled weight_decay = {hyp['weight_decay']}")
